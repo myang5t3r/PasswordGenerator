13fd8516d10fa5b537545f962b550e01b93c53d2
myang5t3r <matthew.yang@hunterdouglas.com>
2022-08-02 23:21:43 -0600

    Created generatePassword fucnction, created variable passwordlength and added check for isNan and lengh requiremt

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 // Assignment code here
+const generatePassword = ()=> {
+  
+  var passwordLength = prompt("Enter a numeric length for your password, must be between 8 and 128 characters")
+  // make into a number so we can check if the we have the right length
+  passwordLength = Number(passwordLength)
+  // Also check if a string was enter into the prompt
+  if (Number.isNaN(passwordLength)) {
+    alert("Please enter a number")
+  }
+  if(passwordLength < 8 || passwordLength > 128){
+    alert("Password must be between 8 and 128 characters")
+    console.log(typeof passwordLength)
+  }
+  // Ask user if password should contain these
+  const passwordTypes = ["lowercase", "uppercase", "numeric", "Special Characters"]
+  passwordTypes.forEach(Element =>  
+    Element = prompt() 
+    );
+
+}
 
 
 // Get references to the #generate element
