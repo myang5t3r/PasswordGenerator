2b0a6ce4f876c16a6bcaa17961de5db00196a06a
myang5t3r <matthew.yang@hunterdouglas.com>
2022-08-03 23:29:43 -0600

    Add condition for which characters to use and generate password code

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 // Assignment code here
 const generatePassword = ()=> {
+  // Global variables
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase ="ABCDEFGHIJKLMNOPWXYZ"
+  var numbers = "0123456789";
+  var specicalCharacters = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~"; 
+  var char ="";
+  var password = "";
   var passwordLength = prompt("Enter a numeric length for your password, must be between 8 and 128 characters")
+  // Create object to store character types
+  const types = {"lowercase":"",
+  "uppercase":"", "numbers":"", "specialCharacters":""};
   // turn into a number so we can check if the we have the right length
-  passwordLength = Number(passwordLength)
+  passwordLength = Number(passwordLength);
   // Also check if a string was enter into the prompt
   if (Number.isNaN(passwordLength)) {
     alert("Please enter a number")
   } 
   else if(passwordLength < 8 || passwordLength > 128){
-    alert("Password must be between 8 and 128 characters")
+    alert("Password must be between 8 and 128 characters");
   }
   else{
-    // Create object to store character types
-    let types = {"lowercase":"",
-    "uppercase":"", "numbers":"", "specialCharacters":""}
     for (let property in types){
     types[property] =prompt(`Would you like your password to contain ${property}, please answer yes or no. `)
     // make lower case incase user 
@@ -25,25 +32,42 @@ const generatePassword = ()=> {
         break;
       }
       // Check if at least one character type is selected 
-      if(types.uppercase === "no"){
-        if(types.lowercase === "no"){
-          if(types.specialCharacters === "no"){
-            if(types.numbers === "no"){
-              alert("No characters selected, you must choose at least one type of character")
-            }
+    if(types.uppercase === "no"){
+      if(types.lowercase === "no"){
+        if(types.specialCharacters === "no"){
+          if(types.numbers === "no"){
+            alert("No characters selected, you must choose at least one type of character")
           }
         }
       }
-    }
-    console.log(types)
-    }
-    }
+    }}}}
+
+  // Conditions for which characters the use chose, if yes append to char string
+  if(Object.values(types)[0] === "yes"){
+    char = char.concat(lowercase)
+  }
+  if(Object.values(types)[1] === "yes"){
+    char = char.concat(uppercase)
+  }
+  if(Object.values(types)[2] === "yes"){
+    char = char.concat(numbers)
+  }
+  if(Object.values(types)[3] === "yes"){
+    char = char.concat(specicalCharacters)
+  }
+  // https://dev.to/code_mystery/random-password-generator-using-javascript-6a
+  // Generate password
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * char.length);
+    password += char.substring(randomNumber, randomNumber +1);
+   }
+
+   return password;
 }
 
- 
-   // Ask user if password should contain these
-  
 
+
+ 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
